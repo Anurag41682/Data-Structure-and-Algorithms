a2f540c2c578702797d8b1fdb8599cafde5afc2b
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2022-07-27 15:45:45 +0530

    Add files via upload

---
+++ Fundamentals/Number-Theory/binaryExponentiation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define dbg(x...)
+#endif
+#define endl '\n'
+#define int long long
+using namespace std;
+int binaryExponentiation(int base, int pow) {
+    int res = 1;
+    while (pow) {
+        if (pow & 1) res *= base;
+        pow >>= 1;
+        base *= base;
+    }
+    return res;
+}
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << binaryExponentiation(a, b);
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+}
