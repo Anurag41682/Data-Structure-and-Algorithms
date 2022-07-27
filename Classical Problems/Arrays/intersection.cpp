b7cbcc234655c51f1853dc38d3e3f27636a0017b
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2022-07-27 15:41:52 +0530

    Update intersection.cpp

--- Classical Problems/Arrays/intersection.cpp
+++ Classical Problems/Arrays/intersection.cpp
@@ -2,34 +2,7 @@
 using namespace std;
 int main()
 {
-    //function to return 
-5 minutes ago
-MaxAndMinIn_Array.cpp
-Add files via upload
-5 minutes ago
-MaxDifferenceHeight_I.cpp
-Add files via upload
-5 minutes ago
-ReverseTheArray.cpp
-Add files via upload
-5 minutes ago
-cyclically_rotate_an_array_by_one.cpp
-Add files via upload
-5 minutes ago
-intersection.cpp
-Add files via upload
-5 minutes ago
-maxDifferenceHeight_II.cpp
-Add files via upload
-5 minutes ago
-moving_all_negativNoin_left.cpp
-Add files via upload
-5 minutes ago
-union.cpp
-Add files via upload
-5 minutes ago
-zeroOneTwosort.cpp
-Add files via uploadthe no. of intersection in two arrays
+    //function to return the no. of intersection in two arrays
     int n;cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
