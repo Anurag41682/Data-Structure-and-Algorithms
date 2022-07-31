3a6da027a44e210edd4b6f710788a631c75f2791
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2022-07-31 12:00:34 +0530

    Update and rename LCM.cpp to LeastCommonMultiple.cpp

--- Fundamentals/Number-Theory/LeastCommonMultiple.cpp
+++ Fundamentals/Number-Theory/LeastCommonMultiple.cpp
@@ -6,7 +6,8 @@
 #define int long long
 using namespace std;
 int GCD(int a, int b) { return b ? GCD(b, a % b) : a; }
-int LCM(int a, int b) { return (a * b) / GCD(a, b); }
+//relation between lcm and hcf of two numbers a and b -> a * b = lcm(a, b) * hcf(a, b)(logic behind the function)
+int LCM(int a, int b) { return (a * b) / GCD(a, b); }  
 void solve() {
     // lcm of two numbers;
     // Time complexity O(log(min(a,b)))
