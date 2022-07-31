a65e2bea8fbc0f7a0a06fe3cf72e5c49ecec0cd2
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2022-07-31 11:57:29 +0530

    Add files via upload

---
+++ Fundamentals/Number-Theory/LCM.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define dbg(x...)
+#endif
+#define endl '\n'
+#define int long long
+using namespace std;
+int GCD(int a, int b) { return b ? GCD(b, a % b) : a; }
+int LCM(int a, int b) { return (a * b) / GCD(a, b); }
+void solve() {
+    // lcm of two numbers;
+    // Time complexity O(log(min(a,b)))
+    int a = 3, b = 9;
+    cout << LCM(a, b) << endl;
+
+    // lcm of multiple numbers
+    // Time complextiy O(n*log(min(all the elements of array))) n is the size of
+    // array
+    vector<int> v = {2, 4, 6, 18};
+    int ans = v[0];
+    for (int i = 0; i < (int)v.size(); i++) {
+        ans = LCM(ans, v[i]);
+    }
+    cout << ans << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    while (T--) solve();
+}
