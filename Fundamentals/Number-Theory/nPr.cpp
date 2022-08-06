e16b19ffabc09cc6a3b9d60addcbd254899f8e3c
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2022-08-06 17:59:29 +0530

    Add files via upload

---
+++ Fundamentals/Number-Theory/nPr.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define dbg(x...)
+#endif
+#define endl '\n'
+#define int long long
+using namespace std;
+int nPr(int n, int r) {
+    int x = 1;
+    while (n - r) {
+        x *= n;
+        n--;
+    }
+    return x;
+}
+void solve() {
+    int n, r;
+    cin >> n >> r;
+    cout << nPr(n, r) << endl;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;  // cin>>T;
+    while (T--) solve();
+}
