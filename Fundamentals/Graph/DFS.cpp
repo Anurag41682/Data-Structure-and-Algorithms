26a0f4988b80733eaf038603f1ee44139c76888d
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2022-08-12 09:44:33 +0530

    Add files via upload

---
+++ Fundamentals/Graph/DFS.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define dbg(x...)
+#endif
+#define endl '\n'
+#define int long long
+using namespace std;
+vector<vector<int>> adj;
+vector<bool> vis;
+void dfs(int node) {
+    if (!vis[node]) {
+        vis[node] = true;
+        cout << node << " ";
+        for (auto it : adj[node]) {
+            dfs(it);
+        }
+    }
+}
+void solve() {
+    // vertex(n) and edge(m)
+    int n, m;
+    cin >> n >> m;
+    adj = vector<vector<int>>(n + 1);
+    vis = vector<bool>(n + 1, false);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    dfs(1);  // to visit every component we can use for loop here it has only
+             // one so no need for it
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    while (T--) solve();
+}
+/*
+
+Input:
+9 9
+1 2
+2 4
+4 5
+5 6
+6 9
+8 9
+8 7
+7 3
+3 2
+
+Graph:
+1-2-4-5-6
+  |     |
+  3-7-8-9
+
+*/
