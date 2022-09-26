8affec4db3c942770803f653043c29dd32c4b1f6
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2022-09-27 01:16:16 +0530

    Update Matrix_Exponentation.cpp

--- Fundamentals/Number-Theory/Matrix_Exponentation.cpp
+++ Fundamentals/Number-Theory/Matrix_Exponentation.cpp
@@ -1,7 +1,7 @@
 /*
 We know Fibonacci sequence is given by:
 f(n)= f(n-1)+f(n-2);
-where f(1) = f(2) = 1; // base case
+where f(1) = 0, f(2) = 1; // base case
 So by matrix multiplication properties we can write it as:
 |f(n)|    =  |1 1| |f(n-1)|
                    |f(n-2)|
@@ -20,8 +20,8 @@ Let us take n=4
 |f(3)|   |1 0| |f(2)|
 
 Let us take n=3
-|f(3)| = |1 1| |f(2)|= |1 1| |1| // since f(2) = f(1) =1 base case
-|f(2)|   |1 0| |f(1)|= |1 0| |1|
+|f(3)| = |1 1| |f(2)|= |1 1| |1| // since f(2) = 1 ,f(1) = 0 base case
+|f(2)|   |1 0| |f(1)|= |1 0| |0|
 
 
 Substituting value of |f(3)| in |f(4)| We get: |f(4)| = |1 1| |1 1| |f(2)|
@@ -40,3 +40,53 @@ So the general formula become:
 |f(n)|  =   t^(n-2) |f(2)|
 |f(n-1) =           |f(1)|
 */
+
+
+
+#include <bits/stdc++.h>
+#ifndef LOCAL
+#define dbg(x...)
+#endif
+#define endl '\n'
+#define int long long
+using namespace std;
+vector<vector<int>> multiply(vector<vector<int>>& a, vector<vector<int>>& b) {
+    int n = a.size();
+    vector<vector<int>> temp(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                temp[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    return temp;
+}
+int fib(int n) {
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+    vector<vector<int>> res(2, vector<int>(2));
+    res[0][0] = res[1][1] = 1;  // initialization with identity matrix so that result remain same after first multiplication.
+    n -= 2;
+    vector<vector<int>> ans(2, vector<int>(2));
+    ans[0][0] = ans[0][1] = ans[1][0] = 1;  //initialization of t  i.e. constant term
+    while (n) {
+        if (n & 1) {
+            res = multiply(res, ans);
+        }
+        ans = multiply(ans, ans);
+        n >>= 1;
+    }
+    return res[0][0];
+}
+void solve() {
+    int n;
+    cin >> n;
+    cout << fib(n);
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T = 1;
+    while (T--) solve();
+}
