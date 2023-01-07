d220d0388620894f8e4196f5338da27fce7bc28d
Anurag41682 <82538589+Anurag41682@users.noreply.github.com>
2023-01-07 16:17:10 +0530

    Update extendedEuclidian.cpp

--- Fundamentals/Number-Theory/extendedEuclidian.cpp
+++ Fundamentals/Number-Theory/extendedEuclidian.cpp
@@ -1,30 +1,28 @@
-// extended eucledian theorem is used to find x and y
-// from the equation ax + by = gcd(a,b)
-// where a and b are given
+// extended eucledian theorem is used to find x and y from the equation ax + by = gcd(a,b) (Bezout's identity) where a and b are given
 
 // ax + by = gcd(a,b)---(i)
-// since gcd(a,b) == gcd(b,a%b)
+// since gcd(a,b) == gcd(b,a%b) -- euclidean algorithm
 // therefore ax + by = gcd(a,b) can also be written as
-// bx1 + (a%b)y1 = gcd(b,a%b)---- after comparing from (i) and right term of
-// this equation, we suppose new x and y, x1 and y1 so that equation remain
-// overall balanced
-// since a%b == a-ceil(a/b)*b
-// bx1 + (a-ceil(a/b)*b)y1 = gcd(b,a%b) = gcd(a,b)
-// bx1 + ay1 - ceil(a/b)*y1 = gcd(a,b) bx1 - ceil(a/b)*y1
-// + ay1 = gcd(a,b) b(x1- ceil(a/b)*y1) + ay1 = gcd(a,b) ay1 + b(x1-
-// ceil(a/b)*y1) = gcd(a,b) --- (ii) after comparing (ii) from (i) we get
-// x=y1 and y=(x1-ceil(a/b)*y1) ---(iii)
+// bx1 + (a%b)y1 = gcd(b,a%b) -- after comparing the right term of this equation from (i), We suppose new x and y, x1 and y1 so that equation remain
+//                               overall balanced
+// since a % b == a-floor(a/b)*b -- this is what modulus is
+// bx1 + (a-floor(a/b)*b)y1 = gcd(b,a%b) = gcd(a,b)
+// bx1 + ay1 - floor(a/b)*b*y1 = gcd(a,b)
+// bx1 - floor(a/b)*b*y1 + ay1 = gcd(a,b)
+// b(x1- floor(a/b)*y1) + ay1 = gcd(a,b) 
+// ay1 + b(x1-floor(a/b)*y1) = gcd(a,b) --- (ii) after comparing (ii) from (i) we get
+// x=y1 and y=(x1-ceil(a/b)*y1) ---(iii) this is the required thing to find the x and y
+// here x and y are of parent equation (which we need to find) and x1 and y1 (used to find x and y) are of child equation.
+
 
 #include <bits/stdc++.h>
 
-pair<int, int> extendedGCD(int a, int b) {  // this function find x and y for
-    if (b == 0) {                           // give a and b
+pair<int, int> extendedGCD(int a, int b) {  // this function find x and y for give a and b
+    if (b == 0) {                          
         return {1, 0};
     }
-    auto t = extendedGCD(
-        b, a % b);  // calling for next subproblem to get its x and y
-    int x =
-        t.second;  // so that we can use equation (iii) to find current x and y
+    auto t = extendedGCD(b, a % b);  // calling for next subproblem to get its x and y so that we can use equation (iii) to find current x and y
+    int x = t.second;               
     int y = t.first - (a / b) * t.second;
     return {x, y};
 }
